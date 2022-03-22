999befbe3060db0a49aedc8331f1e73ca68ee896
Nimesh Maharjan <maharjannimesh11@gmail.com>
2022-03-22 20:07:09 +0545

    done

--- Database_Coursework_MVC/Services/StudentAssignmentService.cs
+++ Database_Coursework_MVC/Services/StudentAssignmentService.cs
@@ -53,7 +53,10 @@ namespace Database_Coursework_MVC.Services
                     con.Open();
                     cmd.Connection = con;
                     cmd.BindByName = true;
-                    cmd.CommandText = "select student_module.module_code, student_module_assignment.assignment_type, student_module_assignment.grade, student_module.student_id, student_module_assignment.status from student_module, student_module_assignment where student_module_assignment.module_code = student_module.module_code and student_module_assignment.student_id = " + studentId + "";
+                    cmd.CommandText = "select module.module_code, student_module_assignment.assignment_type, student_module_assignment.grade," +
+                        " student_module_assignment.student_id, student_module_assignment.status" +
+                        " from module, student_module_assignment" +
+                        " where student_module_assignment.module_code = module.module_code and student_module_assignment.student_id = " + studentId + "";
                     OracleDataReader rdr = cmd.ExecuteReader();
                     while (rdr.Read())
                     {
